672c90652a8179f5230613b7e35b067b6f5acf31
MVCFirst <gokhanay@GOKHANAY>
2018-12-07 15:51:13 +0300

    ViewData ViewBack TempData

---
+++ PassingDataFromControllersToView/Controllers/MyHomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassingDataFromControllersToView.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        // GET: MyHome
+        public ActionResult Index()
+        {
+            ViewData["Title"] = "Ilk Mvc Projesi";
+            ViewBag.Title = "Ilk Mvc Projesi";
+            ViewBag.Numbers = new int[] {1,2,3,4,5 };
+            TempData["message"] = "Message from TempData";
+            return RedirectToAction("Index2");
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
