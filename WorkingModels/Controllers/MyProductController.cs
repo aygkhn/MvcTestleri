21c98e115278664949c77f271040d084d177656d
MVCFirst <gokhanay@GOKHANAY>
2018-12-07 15:17:49 +0300

    Ilk Model View calisması yapildi.

---
+++ WorkingModels/Controllers/MyProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkingModels.Models;
+using WorkingModels.Models.MyProductModels;
+
+namespace WorkingModels.Controllers
+{
+    public class MyProductController : Controller
+    {
+        // GET: MyPrudoct
+        public ActionResult Index()
+        {
+            using (var context = new NorthwindContext())
+            {
+                List<Product> product = context.Products.ToList();
+                List<Category> category = context.Categories.ToList();
+                var model = new MyProductIndexViewModel {
+                    Categories = category,
+                    Products = product
+                };
+                return View(model);
+            }
+
+        }
+        public ActionResult Demo()
+        {
+            return View(model: "Salih");
+        }
+    }
+}
---
+++ WorkingModels/Models/MyProductModels/MyProductIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkingModels.Models.MyProductModels
+{
+    public class MyProductIndexViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
