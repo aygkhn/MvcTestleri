bc44ca0b9b5c172108ebd5ef562372b70c40e3e9
MVCFirst <gokhanay@GOKHANAY>
2018-12-07 16:05:24 +0300

    RouteData RouteData.Value

---
+++ ModelBinding/Controllers/MyHomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBinding.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        // GET: MyHome
+        public ActionResult Index()
+        {
+            string controller= RouteData.Values["controller"].ToString();
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["categoryId"];
+            return Content(string.Format("{0}/{1}/{2}",controller,action,id));
+        }
+    }
+}
